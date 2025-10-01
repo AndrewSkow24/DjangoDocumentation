b3d761366fd0811697dcec284b6d0560ed7e1c7f
Andrew Skow <anjeie24@mail.ru>
2025-10-01 06:19:49 +0300

    19of2900

--- get_started/news/views.py
+++ get_started/news/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.template.context_processors import request
+
+from .models import Article
 
 # Create your views here.
+
+def year_archive(request, year):
+    a_list = Article.objects.filter(pub_date__year=year)
+    print(a_list)
+    context = {"year": year, "article_list": a_list}
+    return render(request, 'news/year_archive.html', context)
+
+def month_archive(request, year, month):
+    a_list = Article.objects.filter(pub_date__year=year, pub_date__month=month)
+    content = {
+        "year": year,
+        "month": month,
+        "article_list": a_list
+    }
+    return render(request, 'news/year_month_archive.html', content)
+
+def article_detail(request, pk):
+    article = Article.objects.filter(pk=pk)
+    content = {
+        "pk": pk,
+        "article": article
+    }
+    return render(request, 'news/details.html', content)
